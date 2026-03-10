1f35080976f57ca9a56507b941533b967f604345
Johnfonx8 <146359517+Johnfonx8@users.noreply.github.com>
2026-03-10 09:12:40 +0700

    import_goldpc&&vip
    import_goldpc&&vip update vip
    
    update vip premium
    
    edit premium v.2
    
    edit premium v.3
    
    Fix hateffect premium
    
    Fix premium && src/config/core.hpp max chare

--- sql-files/main.sql
+++ sql-files/main.sql
@@ -796,7 +796,7 @@ CREATE TABLE IF NOT EXISTS `login` (
 -- added standard accounts for servers, VERY INSECURE!!!
 -- inserted into the table called login which is above
 
-INSERT INTO `login` (`account_id`, `userid`, `user_pass`, `sex`, `email`) VALUES ('1', 's1', 'p1', 'S','athena@athena.com');
+INSERT INTO `login` (`account_id`, `userid`, `user_pass`, `sex`, `email`) VALUES ('1', 'svRagnarokUs', 'svRagnarokPw', 'S','athena@athena.com');
 
 --
 -- Table structure for table `mail`
