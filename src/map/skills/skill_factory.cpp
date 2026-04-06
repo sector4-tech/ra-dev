dc6b283c14e5572cf5055990516abbc340fa6c53
Johnfonx8 <146359517+Johnfonx8@users.noreply.github.com>
2026-04-06 22:29:50 +0700

    Revert "add Druid / Karnos / Alitea Class Implementation"
    This reverts commit 587f5af817e64b5bb7906efcd0eab7f7391db8ca.

--- src/map/skills/skill_factory.cpp
+++ src/map/skills/skill_factory.cpp
@@ -16,7 +16,6 @@
 #include "./acolyte/skill_factory_acolyte.cpp"
 #include "./archer/skill_factory_archer.cpp"
 #include "./custom/skill_factory_custom.cpp"
-#include "./druid/skill_factory_druid.cpp"
 #include "./elemental/skill_factory_elemental.cpp"
 #include "./gunslinger/skill_factory_gunslinger.cpp"
 #include "./homunculus/skill_factory_homunculus.cpp"
@@ -50,7 +49,6 @@ std::unique_ptr<const SkillImpl> SkillFactoryImpl::create(const e_skill skill_id
 		std::make_shared<SkillFactoryNovice>(),
 		std::make_shared<SkillFactoryOther>(),
 		std::make_shared<SkillFactorySummoner>(),
-		std::make_shared<SkillFactoryDruid>(),
 		std::make_shared<SkillFactorySwordman>(),
 		std::make_shared<SkillFactoryTaekwon>(),
 		std::make_shared<SkillFactoryThief>(),
